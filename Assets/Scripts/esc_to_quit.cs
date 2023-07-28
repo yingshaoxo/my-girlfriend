580dbd3ece6cfa5440a92a2970dfa120c257c450
yingshaoxo <yingshaoxo@gmail.com>
2023-07-28 20:34:16 +0800

    refactore

---
+++ Assets/Scripts/esc_to_quit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class esc_to_quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+        #if UNITY_EDITOR
+            if (EditorApplication.isPlaying)
+            {
+                EditorApplication.isPlaying = false;
+            }
+
+        #else 
+            Application.Quit();
+        #endif
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# my-girlfriend
+Just a unity3d game I made for myself.
+
+
+### Requirements
+
+Unity-2021.3.27
+
+Ubuntu
+
+### Support
+
+Linux
+
+Android
+
+### Scrennshot
+
+![welcome](Screenshots/welcome.png "welcome")
